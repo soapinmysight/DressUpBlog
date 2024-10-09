762bb76aa67aff61c97b4133590ab570034e45d6
soapinmysight <dieuwvr@gmail.com>
2024-10-09 14:19:10 +0200

    Added routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about-us', function() {
+    return view('about-us');});
+
+Route::get('/test', [AboutUsController::class, 'index']);
+
+Route::get('/home/{name}', [\App\Http\Controllers\HomeController::class, 'index']);
+
+Route::get('/contact', [ContactController::class, 'index']);
+
