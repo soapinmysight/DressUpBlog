9dee95f130da91d0aeab36ff8786a575ed220594
soapinmysight <dieuwvr@gmail.com>
2024-10-17 17:02:42 +0200

    Asked for help on the relationship between comment and blog, the comments are being shown in blog\show.blade.php. Next: creating comments in view (just like theme\create.blade.php)

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -3,10 +3,14 @@
         <h1>{{ $blog->title }}</h1>
         <p>{{ $blog->description }}</p>
     </div>
-    <form method="POST" action="{{route('Comment.store')}}">
-        @csrf
-        <label for="content">comment</label>
-        <input type="text" id="content" name="content">
-        <button type="submit">Save</button>
-    </form>
+{{--    <form method="POST" action="{{route('comment.store')}}">--}}
+{{--        @csrf--}}
+{{--        <label for="content">comment</label>--}}
+{{--        <input type="text" id="content" name="content">--}}
+{{--        <button type="submit">Save</button>--}}
+{{--    </form>--}}
+
+    @foreach($blog->comments as $comment)
+            <p>{{ $comment->content }}</p>
+    @endforeach
 </x-app-layout>
